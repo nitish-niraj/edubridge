63835ce3f06f9ddc95a1aed302151cfc52c8fab0
nitish <kumarnitish87461@gmail.com>
2026-04-30 23:49:52 +0530

    Enhance code structure and optimize performance

---
+++ app/Http/Requests/Api/TeacherOnlyRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TeacherOnlyRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()?->isTeacher() === true;
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+}
+
