c7440268750ce2528d363967238c88a743968ed2
nitish <kumarnitish87461@gmail.com>
2026-05-08 01:27:59 +0530

    feat: add sitemap.blade.php template for XML sitemap generation

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,5 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<?php echo '<?xml-stylesheet type="text/xsl" href="' . url('/sitemap.xsl') . '"?>'; ?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+{!! '<?xml-stylesheet type="text/xsl" href="' . url('/sitemap.xsl') . '"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($urls as $url)
     <url>
